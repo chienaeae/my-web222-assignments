987c9a475d6e611aa697ea3a4c7edce2c966c511
chienaeae <chienaeae@gmail.com>
2024-07-07 22:04:03 -0400

    complete a5

---
+++ a5/src/js/artists.js
@@ -0,0 +1,44 @@
+/**
+ * WEB222 – Assignment 05
+ *
+ * I declare that this assignment is my own work in accordance with
+ * Seneca Academic Policy. No part of this assignment has been
+ * copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Please update the following with your information:
+ *
+ *      Name:       Chia-Yu Chien
+ *      Student ID: 157049222
+ *      Date:       2024/06/14
+ */
+
+window.artists = [
+  /* TODO */
+  {
+    artistId: "AID-51234",
+    name: "Taylor Swift",
+    urls: [
+      { url: "https://www.taylorswift.com/", name: "Website" },
+      { url: "https://www.instagram.com/taylorswift/", name: "Instagram" },
+      { url: "https://x.com/taylorswift13", name: "Twitter" }
+    ]
+  },
+  {
+    artistId: "AID-51235",
+    name: "Billie Eilish",
+    urls: [
+      { url: "https://shopuk.billieeilish.com/", name: "Store" },
+      { url: "https://www.instagram.com/billieeilish/", name: "Instagram" }
+    ]
+  },
+  {
+    artistId: "AID-51236",
+    name: "The Weeknd",
+    urls: [
+      { url: "https://www.theweeknd.com/", name: "Website" },
+      { url: "https://open.spotify.com/artist/1Xyo4u8uXC1ZmMpatF05PJ", name: "Spotify" },
+      { url: "https://www.instagram.com/theweeknd/", name: "Instagram" }
+    ]
+  }
+];
---
+++ a5/src/js/songs.js
@@ -0,0 +1,281 @@
+/**
+ * WEB222 – Assignment 05
+ *
+ * I declare that this assignment is my own work in accordance with
+ * Seneca Academic Policy. No part of this assignment has been
+ * copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Please update the following with your information:
+ *
+ *      Name:       Chia-Yu Chien
+ *      Student ID: 157049222
+ *      Date:       2024/06/14
+ */
+
+window.songs = [
+  {
+    songId: "SID-13423453",
+    artistId: "AID-51234",
+    title: "Blank Space",
+    imageUrl:
+      "https://images.unsplash.com/photo-1719216325267-71c936d0218e?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2014",
+    duration: 272,
+    url: "https://youtu.be/e-ORhEE9VVg?si=0f0scPLjzgCsxim1",
+    explicit: false
+  },
+  {
+    songId: "SID-13423454",
+    artistId: "AID-51234",
+    title: "Anti-Hero",
+    imageUrl:
+      "https://images.unsplash.com/photo-1720170494675-e2dcd6de34a7?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2022",
+    duration: 309,
+    url: "https://www.youtube.com/watch?v=b1kbLwvqugk",
+    explicit: false
+  },
+  {
+    songId: "SID-13423455",
+    artistId: "AID-51234",
+    title: "Look What You Made Me Do",
+    imageUrl:
+      "https://images.unsplash.com/photo-1720170494675-e2dcd6de34a7?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2017",
+    duration: 255,
+    url: "https://www.youtube.com/watch?v=3tmd-ClpJxA",
+    explicit: true
+  },
+  {
+    songId: "SID-13423456",
+    artistId: "AID-51234",
+    title: "Bad Blood",
+    imageUrl:
+      "https://images.unsplash.com/photo-1692607519784-9e5406625d00?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2015",
+    duration: 244,
+    url: "https://www.youtube.com/watch?v=QcIy9NiNbmo",
+    explicit: false
+  },
+  {
+    songId: "SID-13423457",
+    artistId: "AID-51234",
+    title: "Delicate",
+    imageUrl:
+      "https://images.unsplash.com/photo-1719968004641-deffe630c532?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2018",
+    duration: 234,
+    url: "https://www.youtube.com/watch?v=tCXGJQYZ9JA",
+    explicit: false
+  },
+  {
+    songId: "SID-13423458",
+    artistId: "AID-51234",
+    title: "Cruel Summer",
+    imageUrl:
+      "https://images.unsplash.com/photo-1720169676311-d97490bace3f?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2019",
+    duration: 180,
+    url: "https://www.youtube.com/watch?v=GrKQvyXpNgc",
+    explicit: false
+  },
+  {
+    songId: "SID-13423459",
+    artistId: "AID-51234",
+    title: "Karma",
+    imageUrl:
+      "https://images.unsplash.com/photo-1720286790380-f773528fda9a?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2023",
+    duration: 205,
+    url: "https://www.youtube.com/watch?v=XzOvgu3GPwY",
+    explicit: false
+  },
+  {
+    songId: "SID-13423460",
+    artistId: "AID-51234",
+    title: "Lavender Haze",
+    imageUrl:
+      "https://images.unsplash.com/photo-1720247522689-d851331da090?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2023",
+    duration: 210,
+    url: "https://www.youtube.com/watch?v=h8DLofLM7No",
+    explicit: true
+  },
+  {
+    songId: "SID-13423461",
+    artistId: "AID-51234",
+    title: "You Need To Calm Down",
+    imageUrl:
+      "https://images.unsplash.com/photo-1720155243642-313849a7027b?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2019",
+    duration: 210,
+    url: "https://www.youtube.com/watch?v=Dkk9gvTmCXY",
+    explicit: false
+  },
+  {
+    songId: "SID-13423461",
+    artistId: "AID-51234",
+    title: "Shake It Off",
+    imageUrl:
+      "https://images.unsplash.com/photo-1720233833466-64415765eaf7?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2017",
+    duration: 265,
+    url: "https://www.youtube.com/watch?v=kNlX5C3Ubn4",
+    explicit: false
+  },
+  {
+    songId: "SID-13423463",
+    artistId: "AID-51234",
+    title: "But Daddy I Love Him",
+    imageUrl:
+      "https://images.unsplash.com/photo-1720247522770-5d0e62edf619?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2024",
+    duration: 344,
+    url: "https://www.youtube.com/watch?v=U2W173hRfyA",
+    explicit: false
+  },
+  {
+    songId: "SID-13423464",
+    artistId: "AID-51234",
+    title: "Red",
+    imageUrl:
+      "https://images.unsplash.com/photo-1720247522780-db8ba86cbfef?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2013",
+    duration: 241,
+    url: "https://www.youtube.com/watch?v=Zlot0i3Zykw",
+    explicit: false
+  },
+  {
+    songId: "SID-13423465",
+    artistId: "AID-51234",
+    title: "willow",
+    imageUrl:
+      "https://images.unsplash.com/photo-1719216324621-af3512ec2766?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2020",
+    duration: 252,
+    url: "https://www.youtube.com/watch?v=RsEZmictANA",
+    explicit: false
+  },
+  {
+    songId: "SID-13423466",
+    artistId: "AID-51234",
+    title: "Back To December",
+    imageUrl:
+      "https://images.unsplash.com/photo-1719321920092-b02ca3c27d1f?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2011",
+    duration: 304,
+    url: "https://www.youtube.com/watch?v=QUwxKWT6m7U",
+    explicit: false
+  },
+  {
+    songId: "SID-13423467",
+    artistId: "AID-51234",
+    title: "Clean",
+    imageUrl:
+      "https://images.unsplash.com/photo-1719937075949-83b1f1ee3e42?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2023",
+    duration: 276,
+    url: "https://www.youtube.com/watch?v=AppsjTInqiw",
+    explicit: false
+  },
+  {
+    songId: "SID-13423468",
+    artistId: "AID-51234",
+    title: "betty",
+    imageUrl:
+      "https://images.unsplash.com/photo-1720121990316-b74068f20a3a?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2020",
+    duration: 295,
+    url: "https://www.youtube.com/watch?v=6TAPqXkZW_I",
+    explicit: false
+  },
+  {
+    songId: "SID-13423469",
+    artistId: "AID-51234",
+    title: "exile",
+    imageUrl:
+      "https://images.unsplash.com/photo-1719865632422-2dc7ada8d84b?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2020",
+    duration: 286,
+    url: "https://www.youtube.com/watch?v=osdoLjUNFnA",
+    explicit: false
+  },
+  {
+    songId: "SID-13423470",
+    artistId: "AID-51234",
+    title: "All Too Well",
+    imageUrl:
+      "https://images.unsplash.com/photo-1720010944703-48cb41c457f5?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2021",
+    duration: 330,
+    url: "https://www.youtube.com/watch?v=9OQBDdNHmXo",
+    explicit: false
+  },
+  {
+    songId: "SID-13423471",
+    artistId: "AID-51234",
+    title: "Sparks Fly",
+    imageUrl:
+      "https://images.unsplash.com/photo-1719922327925-ac8cd949dffe?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2011",
+    duration: 270,
+    url: "https://www.youtube.com/watch?v=oKar-tF__ac",
+    explicit: false
+  },
+  {
+    songId: "SID-13423472",
+    artistId: "AID-51235",
+    title: "CHIHIRO",
+    imageUrl:
+      "https://images.unsplash.com/photo-1719591486736-7eb980c3f884?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2024",
+    duration: 323,
+    url: "https://www.youtube.com/watch?v=BY_XwvKogC8",
+    explicit: false
+  },
+  {
+    songId: "SID-13423473",
+    artistId: "AID-51235",
+    title: "What Was I Made For?",
+    imageUrl:
+      "https://images.unsplash.com/photo-1720065359628-1a9d36822df7?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2023",
+    duration: 248,
+    url: "https://www.youtube.com/watch?v=cW8VLC9nnTo",
+    explicit: false
+  },
+  {
+    songId: "SID-13423474",
+    artistId: "AID-51235",
+    title: "lovely",
+    imageUrl:
+      "https://images.unsplash.com/photo-1719938571041-daee17be1bb9?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2018",
+    duration: 200,
+    url: "https://www.youtube.com/watch?v=V1Pl8CzNzCw",
+    explicit: false
+  },
+  {
+    songId: "SID-13423475",
+    artistId: "AID-51236",
+    title: "Save Your Tears",
+    imageUrl:
+      "https://images.unsplash.com/photo-1719216327634-55b8b36f9114?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2021",
+    duration: 248,
+    url: "https://www.youtube.com/watch?v=XXYlFuWEuKI",
+    explicit: false
+  },
+  {
+    songId: "SID-13423476",
+    artistId: "AID-51236",
+    title: "Is There Someone Else?",
+    imageUrl:
+      "https://images.unsplash.com/photo-1719216323962-ea9b315ad9bd?auto=format&fit=crop&w=540&h=480&q=80",
+    year: "2023",
+    duration: 254,
+    url: "https://www.youtube.com/watch?v=1XqIWr_WqM4",
+    explicit: false
+  }
+];
